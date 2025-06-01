cf038013a33a30d047c5871ab0b9361640f4f55b
c0de4un <code4un@yandex.ru>
2025-06-01 15:41:48 +0400

    Register routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\CancelNumberAction;
+use App\Http\Controllers\API\GetNumberAction;
+use App\Http\Controllers\API\GetSMSAction;
+use App\Http\Controllers\API\GetStatusAction;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/getNumber', [GetNumberAction::class, 'index']);
+Route::get('/getSms', [GetSMSAction::class, 'index']);
+Route::get('/cancelNumber', [CancelNumberAction::class, 'index']);
+Route::get('/getStatus', [GetStatusAction::class, 'index']);
